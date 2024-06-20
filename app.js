0dec05e73a673855c71215cb49fec45067255122
Ilya Lelkov <ilya.lelkov@mail.ru>
2024-06-20 19:02:26 +1000

    create login and logout functional

--- app.js
+++ app.js
@@ -1,16 +1,43 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cookieParser = require('cookie-parser');
+const express = require('express')
+const mongoose = require('mongoose')
+const cookieParser = require('cookie-parser')
+const {register, login} = require('./controllers/user')
+const mapUser = require('./helpers/mapUser')
 
 const port = 3001
 const app = express()
 
-app.use(cookieParser());
-app.use(express.json());
+app.use(cookieParser())
+app.use(express.json())
+
+app.post('/register', async (req, res) => {
+  try {
+    const user = await register(req.body.login, req.body.password)
+    res.send({error: null, user: mapUser(user)})
+  } catch (error) {
+    res.send({error: error.message || 'Unknown error'})
+  }
+})
+
+app.post('/login', async (req, res) => {
+  try {
+    const {user, token} = await login(req.body.login, req.body.password)
+
+    res.cookie('token', token, {httpOnly: true})
+        .send({error: null, user: mapUser(user)})
+  } catch (error) {
+    res.send({error: error.message || 'Unknown error'})
+  }
+})
+
+app.post('/logout',  (req, res) => {
+  res.cookie('token', '', {httpOnly: true})
+      .send({})
+})
 
 mongoose.connect('mongodb+srv://Ilya:Ilyaasasin99@cluster0.buslwrr.mongodb.net/blog?retryWrites=true&w=majority&appName=Cluster0')
-.then(() => {
-  app.listen(port, () => {
-    console.log(`Server started on port ${port}`);
-  });
-})
+    .then(() => {
+      app.listen(port, () => {
+        console.log(`Server started on port ${port}`)
+      })
+    })
---
+++ controllers/user.js
@@ -0,0 +1,36 @@
+const bcrypt = require('bcrypt')
+const User = require('../models/user')
+const generateToken = require('../helpers/generateToken')
+
+async function register(login, password) {
+  if (!password) {
+    throw new Error('Password is empty')
+  }
+
+  const passwordHash = await bcrypt.hash(password, 10)
+
+  const user = await User.create({login, password: passwordHash})
+
+  return user
+}
+
+async function login(login, password) {
+  const user = await User.findOne({login})
+
+  if (!user) {
+    throw new Error('User not found')
+  }
+
+  const isPasswordMatch = await bcrypt.compare(password, user.password)
+
+  if (!isPasswordMatch) {
+    throw new Error('Wrong password')
+  }
+
+  const token = generateToken({id: user.id})
+
+  return {token, user}
+}
+
+
+module.exports = {register, login}
